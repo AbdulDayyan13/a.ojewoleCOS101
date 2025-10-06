5a76a077d0584f005a77c32f4cd02c9469206dcb
AbdulDayyan13 <abduldayyan.ojewole@pau.edu.ng>
2025-10-06 20:26:13 +0100

    (week-2 practice_2)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+	println!(); //prints just a 
+
+	println!("hello "); //prints
+
+	println!("format {} argument" , "some"); //prints format some arguments
+
+	println!("My name is {}. I am a student of COS101 class of 2025", "Ojewole AdulDayyan" );
+}
