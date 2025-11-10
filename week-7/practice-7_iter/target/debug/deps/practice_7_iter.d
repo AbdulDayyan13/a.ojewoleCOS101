4fc707921a234e3814196e250b06eeb85a825884
AbdulDayyan13 <abduldayyan.ojewole@pau.edu.ng>
2025-11-10 12:42:00 +0100

    (week-7_practice-7_iter)

---
+++ week-7/practice-7_iter/target/debug/deps/practice_7_iter.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\a.ojewoleCOS101\week-7\practice-7_iter\target\debug\deps\practice_7_iter.d: src\main.rs
+
+C:\Users\NEW\a.ojewoleCOS101\week-7\practice-7_iter\target\debug\deps\practice_7_iter.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice-7_iter/target/debug/practice-7_iter.d
@@ -0,0 +1,1 @@
+C:\Users\NEW\a.ojewoleCOS101\week-7\practice-7_iter\target\debug\practice-7_iter.exe: C:\Users\NEW\a.ojewoleCOS101\week-7\practice-7_iter\src\main.rs
